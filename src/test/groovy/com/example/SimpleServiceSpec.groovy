102d816b8a3d09017f629b1c02cd7ed7706d48a8
zscgrhg <zscgrhg@qq.com>
2019-08-04 09:26:50 +0800

    init

--- src/test/groovy/com/example/SimpleServiceSpec.groovy
+++ src/test/groovy/com/example/SimpleServiceSpec.groovy
@@ -1,22 +1,25 @@
 package com.example
 
-
+import com.github.starnowski.bmunit.extension.junit4.rule.BMUnitMethodRule
 import org.jboss.byteman.contrib.bmunit.BMRule
-import org.jboss.byteman.contrib.bmunit.WithByteman
+import org.junit.Rule
 import spock.lang.Specification
-@WithByteman
-@BMRule(name = "handle file not found",
-        targetClass = "File",
-        targetMethod = "<init>(String)",
-        action = "throw new RuntimeException( \"Ha ha Byteman fooled you again!\" )"
-)
+
+
 class SimpleServiceSpec extends Specification {
 
+    @Rule
+    public BMUnitMethodRule bmUnitMethodRule = new BMUnitMethodRule()
 
-    def test(){
+    @BMRule(name = "handle file not found",
+            targetClass = "File",
+            targetMethod = "<init>(String)",
+            action = "throw new RuntimeException( \"Ha ha Byteman fooled you again!\" )"
+    )
+    def test() {
         File file = new File("C:\\tutorials\\src\\main\\resources\\lwk.txt");
         System.out.println(file.length());
         expect:
-        1==1
+        1 == 1
     }
 }
